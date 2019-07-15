f947c6fe6ee9001de4b090f2b4bd889adb77f53c
damu1997 <damodaran.krishnan@atmecs.com>
2019-07-15 14:08:00 +0530

    added post request

--- RestPratice/src/main/java/com/rest/automation/RestPratice/GETBDDMethod.java
+++ RestPratice/src/main/java/com/rest/automation/RestPratice/GETBDDMethod.java
@@ -1,18 +1,22 @@
 package com.rest.automation.RestPratice;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import groovyjarjarasm.asm.commons.Method;
+
 import io.restassured.RestAssured;
+import io.restassured.http.Method;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.response.Response;
 
 import static org.hamcrest.Matchers.*;
 
+import java.awt.image.RescaleOp;
+
 import static io.restassured.RestAssured.*;
-public class BDDMethod {
+public class GETBDDMethod {
 	
-//@Test
+@Test
 public void test_NumberOfCircuitsFor2017(){
 given().
 when().
@@ -26,7 +30,7 @@ and().
 header("Content-Length", equalTo("4551"));
 }
 
-//@Test
+@Test
 public void test_seasons() {
 given().
 when().	
@@ -39,11 +43,45 @@ log();
 
 }
 @Test
-public void Response() {
+public void ResponseForF1Race() {
 RestAssured.baseURI="http://ergast.com/api/f1/seasons.json";
 RequestSpecification httprequest= RestAssured.given();
 Response response=httprequest.request(io.restassured.http.Method.GET, "http://ergast.com/api/f1/seasons.json");
 String responseBody=response.getBody().asString();
 System.out.print(responseBody);	
 }
+@Test
+public void GetWeatherDetailsValidCity() {
+RestAssured.baseURI="http://restapi.demoqa.com/utilities/weather/city";
+RequestSpecification httprequest=RestAssured.given();
+Response response =httprequest.request(Method.GET,"/hyderabad" );
+String responseBody=response.getBody().asString();
+System.out.print(responseBody);	
 }
+
+//@Test
+public void GetWeatherDetailsInvalidCity()
+{
+	RestAssured.baseURI = "http://restapi.demoqa.com/utilities/weather/city";
+	RequestSpecification httpRequest = RestAssured.given();
+	Response response = httpRequest.get("/78789798798");
+	int statusCode = response.getStatusCode();
+	Assert.assertEquals(statusCode /*actual value*/, 200 /*expected value*/, "Correct status code returned");
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RestPratice/src/main/java/com/rest/automation/RestPratice/Post_request.java
@@ -0,0 +1,55 @@
+package com.rest.automation.RestPratice;
+
+
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class Post_request {
+
+@Test	
+public void RegistrationSuccessful() {
+	
+	  //specify the URL
+	 	RestAssured.baseURI="	http://restapi.demoqa.com/customer" + 
+	 			"";
+	//RequestObjectt
+	RequestSpecification httprequest=RestAssured.given();
+	//Sending along with post request
+	JSONObject requestparam=new JSONObject();
+	requestparam.put("FirstName","Damu");
+	requestparam.put("LastName","CR7");
+	requestparam.put("UserName","DamCR7");
+	requestparam.put("Password","Damu1997");
+	requestparam.put("Email","DamuCR7@gmail.com");
+	httprequest.header("Content-Type","application/json");
+	httprequest.body( requestparam.toJSONString());
+	//response object
+	Response response=httprequest.request(Method.POST,"/register");
+	//print response
+	String responseBody=response.getBody().asString();
+System.out.println("Response body->"+responseBody);
+//status code validation
+int statusCode=response.getStatusCode();
+System.out.println("Status code is: "+statusCode);
+Assert.assertEquals(statusCode, 201);
+
+//success code validation
+String successCode=response.jsonPath().get("SuccessCode");
+Assert.assertEquals(successCode, "OPERATION_SUCCESS");
+
+	
+	
+	
+	
+
+}
+	
+}
